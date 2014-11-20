e7877c59430684da325d7edd1a9e82d2100efcb8
jonathan.richter@kearsarge.org <jonathan.richter@kearsarge.org>
2014-11-20 13:38:53 -0500

    Powerup and text worked on.

--- Powerup.py
+++ Powerup.py
@@ -1,1 +1,13 @@
+import pygame
+
+class Powerup:
+	def__intit__(self,image,pos[0,0])
+		self.image = pygame.image.load(image)
+		self.rect = self.image.get_rect()
+		self.place(pos)
+		self.living = True
+
+def place(self, pos):
+		self.rect.center = pos
+
 
