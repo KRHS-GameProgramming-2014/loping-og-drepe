8b90846dc3de6cbe5d78f8cd558d3cc9fed892d0
noahtckr <noah.tucker@kearsarge.org>
2014-12-18 13:41:04 -0500

    Worked on Player and Game

--- Game.py
+++ Game.py
@@ -13,9 +13,52 @@ size = width, height
 
 
 screen = pygame.display.set_mode(size)
-player = []
-player1 = [Player(1, "ball.png")]
-player1 += [Player(1, "ball.png")]
+players = []
+player1 = [Player(1, "ball.png",[0,0], [100,100])]
+player1 += [Player(1, "ball.png",[0,0], [100,100])]
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				player1.go("up")
+			if event.key == pygame.K_RIGHT:
+				player1.go("right")
+			if event.key == pygame.K_DOWN:
+				player1.go("down")
+			if event.key == pygame.K_LEFT:
+				player1.go("left")
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_UP:
+				player1.go("stop up")
+			if event.key == pygame.K_RIGHT:
+				player1.go("stop right")
+			if event.key == pygame.K_DOWN:
+				player1.go("stop down")
+			if event.key == pygame.K_LEFT:
+				player1.go("stop left")
+
+
+
+	if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_w:
+				player2.go("up")
+			if event.key == pygame.K_d:
+				player2.go("right")
+			if event.key == pygame.K_s:
+				player2.go("down")
+			if event.key == pygame.K_a:
+				player2.go("left")
+	if event.type == pygame.KEYUP:
+			if event.key == pygame.K_w:
+				player2.go("stop up")
+			if event.key == pygame.K_d:
+				player2.go("stop right")
+			if event.key == pygame.K_s:
+				player2.go("stop down")
+			if event.key == pygame.K_a:
+				player2.go("stop left")
 
 
 while True:
@@ -63,71 +106,9 @@ while True:
 		
 	
 
-
-	
-	
-
-pygame.init()
-
-clock = pygame.time.Clock
-
-width = 100
-height = 800
-size = width, height
-
-
-
-screen = pygame.display.set_mode(size)
-
-player1 = [Player(1, "ball.png")]
-
-
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_UP:
-				player1.go("up")
-			if event.key == pygame.K_RIGHT:
-				player1.go("right")
-			if event.key == pygame.K_DOWN:
-				player1.go("down")
-			if event.key == pygame.K_LEFT:
-				player1.go("left")
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_UP:
-				player1.go("stop up")
-			if event.key == pygame.K_RIGHT:
-				player1.go("stop right")
-			if event.key == pygame.K_DOWN:
-				player1.go("stop down")
-			if event.key == pygame.K_LEFT:
-				player1.go("stop left")
-
-
-
-	if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_w:
-				player2.go("up")
-			if event.key == pygame.K_d:
-				player2.go("right")
-			if event.key == pygame.K_s:
-				player2.go("down")
-			if event.key == pygame.K_a:
-				player2.go("left")
-	if event.type == pygame.KEYUP:
-			if event.key == pygame.K_w:
-				player2.go("stop up")
-			if event.key == pygame.K_d:
-				player2.go("stop right")
-			if event.key == pygame.K_s:
-				player2.go("stop down")
-			if event.key == pygame.K_a:
-				player2.go("stop left")
-		
-	
-
-	bgColor = r,g,b
-	screen.fill(bgColor)
+	bgScreen = pygame.image.load("BL1.png")
+	screen.fill(bgScreen)
+	for player1 in players:
+		screen.blit(player1.image, player1.rect)	
 	pygame.display.flip()
 	clock.tick(60)
--- Player.py
+++ Player.py
@@ -1,18 +1,18 @@
 import pygame
 class Player():
-	def __init__(self, playernum, image, speed = [0,0], pos = [100,100]):
+	def __init__(self, playernum, image, speed = [0,0], pos = [0,0]):
 		self.playernumber = playernum
 		if playernum == 1:
 			self.images = []
-		self.image = self.images[self.frame]
-		self.rect = self.image.get_rect(center = self.rect.center)
+		self.image = pygame.image.load(image)
+		self.frame = 0
+		self.rect = self.image.get_rect()
 		self.place(pos)
 		self.speedx = speed[0]
 		self.speedy = speed[1]
 		self.speed = [self.speedx, self.speedy]
 		self.maxSpeed = 10
 		self.changed = False
-		self.frame = 0
 		self.maxFrame = len(self.images) - 1
 		self.waitCount = 0
 		self.maxWait = 60*.25
@@ -24,7 +24,6 @@ class Player():
 
 	def place(self, pos):
 		self.rect.center = pos
-	
 		
 	def go(self, direction):
 		if direction == "up":
