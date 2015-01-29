2375434dfa80fb2771a4a2d11a1680e773fc910f
jonathan.richter@kearsarge.org <jonathan.richter@kearsarge.org>
2015-01-29 13:27:36 -0500

    shoot

--- Powerup.py
+++ Powerup.py
@@ -20,6 +20,8 @@ class Powerup():
 		if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
 			if self.rect.bottom > other.rect.top and self.rect.top < other.rect.bottom:
 				if (self.radius + other.radius) > self.distance(other.rect.center):
+					
+					
 					self.living = False
 		
 	def update(self):
