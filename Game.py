917e2c8c58f9885b7de84ffd1d41226eb0e915d6
noahtckr <noah.tucker@kearsarge.org>
2015-01-29 13:29:04 -0500

    stuff

--- Game.py
+++ Game.py
@@ -1,3 +1,4 @@
+
 import pygame, sys
 from Player import Player
 from Button import Button
@@ -26,95 +27,94 @@ powerups = [Powerup("SPU",[500,600])]
 run = False
 
 startButton = Button([width/2, height-300], 
-				     "Start_Base.png", 
-				     "Start_Clicked.png")
+                     "Start_Base.png", 
+                     "Start_Clicked.png")
 
 while True:
-	while not run:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT: sys.exit()
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RETURN:
-					run = True
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				startButton.click(event.pos)
-			if event.type == pygame.MOUSEBUTTONUP:
-				if startButton.release(event.pos):
-					run = True
-					
-		bgColor = r,g,b
-		screen.fill(bgColor)
-		screen.blit(bgImage, bgRect)
-		screen.blit(startButton.image, startButton.rect)
-		pygame.display.flip()
-		
-		
-	bgImage = pygame.image.load("BL1.png").convert()
-	bgRect = bgImage.get_rect()
-	while run:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT: sys.exit()
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_w:
-					#print "[DEBUG] w button pressed"
-					player1.go("up")
-					player1.update(width, height)
-				if event.key == pygame.K_d:
-					#print "[DEBUG] d button pressed"
-					player1.go("right")
-					player1.update(width, height)
-				if event.key == pygame.K_s:
-					#print "[DEBUG] s button pressed"
-					player1.go("down")
-					player1.update(width, height)
-				if event.key == pygame.K_a:
-					#print "[DEBUG] a button pressed"
-					player1.go("left")
-					player1.update(width, height)
-				if event.key == pygame.K_UP:
-					#print "[DEBUG] up arrow pressed"
-					player2.go("up")
-					player2.update(width, height)
-				if event.key == pygame.K_RIGHT:
-					#print "[DEBUG] right arrow pressed"
-					player2.go("right")
-					player2.update(width, height)
-				if event.key == pygame.K_DOWN:
-					#print "[DEBUG] down arrow pressed"
-					player2.go("down")
-					player2.update(width, height)
-				if event.key == pygame.K_LEFT:
-					#print "[DEBUG] left arrow pressed"
-					player2.go("left")
-					player2.update(width, height)				
-			
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_w:
-					player1.go("stop up")
-				if event.key == pygame.K_d:
-					player1.go("stop right")
-				if event.key == pygame.K_s:
-					player1.go("stop down")
-				if event.key == pygame.K_a:
-					player1.go("stop left")
-				if event.key == pygame.K_UP:
-					player2.go("stop up")
-				if event.key == pygame.K_RIGHT:
-					player2.go("stop right")
-				if event.key == pygame.K_DOWN:
-					player2.go("stop down")
-				if event.key == pygame.K_LEFT:
-					player2.go("stop left")
-		
-		for powerup in powerups:
-			powerup.update()
-										
-		bgColor = r,g,b
-		screen.fill(bgColor)
-		screen.blit(bgImage, bgRect)
-		screen.blit(player1.image, player1.rect)
-		screen.blit(player2.image, player2.rect)
-		for powerup in powerups:
-			screen.blit(powerup.image, powerup.rect)
-		pygame.display.flip()
-		
+    while not run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    run = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                startButton.click(event.pos)
+            if event.type == pygame.MOUSEBUTTONUP:
+                if startButton.release(event.pos):
+                    run = True
+         
+         
+        bgImage = pygame.image.load("Art/Title.png").convert()
+        bgImage = pygame.transform.scale(bgImage, [1000,715])
+        bgRect = bgImage.get_rect()
+        bgColor = r,g,b
+        screen.fill(bgColor)
+        screen.blit(bgImage, bgRect)
+        screen.blit(startButton.image, startButton.rect)
+        pygame.display.flip()
+        
+        
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    #print "[DEBUG] w button pressed"
+                    player1.go("up")
+                if event.key == pygame.K_d:
+                    #print "[DEBUG] d button pressed"
+                    player1.go("right")
+                if event.key == pygame.K_s:
+                    #print "[DEBUG] s button pressed"
+                    player1.go("down")
+                if event.key == pygame.K_a:
+                    #print "[DEBUG] a button pressed"
+                    player1.go("left")
+                if event.key == pygame.K_UP:
+                    #print "[DEBUG] up arrow pressed"
+                    player2.go("up")
+                if event.key == pygame.K_RIGHT:
+                    #print "[DEBUG] right arrow pressed"
+                    player2.go("right")
+                if event.key == pygame.K_DOWN:
+                    #print "[DEBUG] down arrow pressed"
+                    player2.go("down")
+                if event.key == pygame.K_LEFT:
+                    #print "[DEBUG] left arrow pressed"
+                    player2.go("left")               
+            
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    player1.go("stop up")
+                if event.key == pygame.K_d:
+                    player1.go("stop right")
+                if event.key == pygame.K_s:
+                    player1.go("stop down")
+                if event.key == pygame.K_a:
+                    player1.go("stop left")
+                if event.key == pygame.K_UP:
+                    player2.go("stop up")
+                if event.key == pygame.K_RIGHT:
+                    player2.go("stop right")
+                if event.key == pygame.K_DOWN:
+                    player2.go("stop down")
+                if event.key == pygame.K_LEFT:
+                    player2.go("stop left")
+        
+        player1.update(width, height)
+        player2.update(width, height)
+        
+        for powerup in powerups:
+            powerup.update()
+        bgImage = pygame.image.load("BL1.png").convert()
+        bgImage = pygame.transform.scale(bgImage, [1000,715])
+        bgColor = r,g,b
+        screen.fill(bgColor)
+        screen.blit(bgImage, bgRect)
+        screen.blit(player1.image, player1.rect)
+        screen.blit(player2.image, player2.rect)
+        for powerup in powerups:
+            screen.blit(powerup.image, powerup.rect)
+        pygame.display.flip()
+        
